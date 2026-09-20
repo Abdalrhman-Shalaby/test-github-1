578bbefa8cfff96d9908274c9b2639044b39cd89
Ibrahim Shalaby <ebrahimanas2027@gmail.com>
2026-09-20 06:38:30 +0300

    fix: update console.log statements in login.js to include additional names

--- login.js
+++ login.js
@@ -1,2 +1,7 @@
 console.console.log("Abdalrhman ");
 console.console.log("Abdalrhman ");
+console.console.log("ibrahim ");
+console.console.log("Aml ");
+console.console.log("omar");
+console.console.log("anas");
+console.console.log("Hager");
